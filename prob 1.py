a556488bb95cdd06a2b301de38f978e5de80dc4a
alexandra605 <70931822+alexandra605@users.noreply.github.com>
2020-11-10 10:18:41 +0300

    Add files via upload

---
+++ prob 1.py
@@ -0,0 +1,10 @@
+#problema 1
+n=eval(input("introduceti un oarecare numar: "))
+if n==28:
+    print("februarie")
+if n==30:
+    print("aprilie,iunie,septembrie, noiembrie")
+if n==31:
+    print("ianuarie, martie, mai, iulie, august, octombrie,decembrie")
+if n==29:
+     print("februarie an bisect")
