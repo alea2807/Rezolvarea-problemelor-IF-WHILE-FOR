fd5ee8375dd92105363a35b7504a0b503d47d939
alexandra605 <70931822+alexandra605@users.noreply.github.com>
2020-11-17 19:21:06 +0400

    Update prob 7.py

--- prob 7.py
+++ prob 7.py
@@ -1,10 +1,11 @@
-a=int(input("introduceti virsta lui mihai a="))
+v=int(input("introduceti virsta lui mihai:"))
 c=1
-if a<20:
-    for i in range(1, a+1 ):
+if v<20:
+    for i in range(1, v+1 ):
         if i==1:
             c+=2
         else:
             c=(c*2)+i
-print("la aceasta virsta de " ,a, "ani mihai a primit " ,c, "dolari") 
-print("la virsta de 6 ani cadoul lui mihai depaseste suma de 100 de dolari")    
+print("la aceasta virsta de " ,v, "ani mihai a primit " ,c, "dolari") 
+if v==6:
+    print("la virsta de 6 ani cadoul lui mihai depaseste suma de 100 de dolari")    
